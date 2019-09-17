e6945f85e77328c7b732717d7298075c2f3f0589
snail007 <arraykeys@gmail.com>
2019-09-18 06:28:58 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -2,23 +2,19 @@
 F="proxy-admin-free_linux-amd64.tar.gz"
 
 set -e
-if [ -e /tmp/proxy ]; then
-    rm -rf /tmp/proxy
+if [ -e /etc/gpaf ]; then
+    rm -rf /etc/gpaf
 fi
-mkdir /tmp/proxy
-cd /tmp/proxy
+mkdir /etc/gpaf
+cd /etc/gpaf
 LAST_VERSION=$(curl --silent "https://api.github.com/repos/snail007/proxy_admin_free/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 wget "https://github.com/snail007/proxy_admin_free/releases/download/${LAST_VERSION}/$F"
 
 # #install proxy
 tar zxvf $F
-rm /usr/bin/proxy-admin-free
 chmod +x proxy-admin-free
-mv proxy-admin-free /usr/bin/
-/usr/bin/proxy-admin-free uninstall
-/usr/bin/proxy-admin-free install
+./proxy-admin-free uninstall
+./proxy-admin-free install
 rm $F
-
 systemctl  status ProxyAdminFree
-
 echo "install done, please visit : http://YOUR_IP:32080/"
